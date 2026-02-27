5c845bb2adbd2cfbb12643d093858b39c31a09a1
NOPE-BDB <3241410869@qq.com>
2026-02-27 23:10:42 +0800

    Fix game list refresh issue

--- push.ps1
+++ push.ps1
@@ -1,8 +1,5 @@
 cd c:\Users\witma\Documents\TRAE\railway
 $env:Path = "C:\Program Files\Git\bin;$env:Path"
-git init
 git add .
-git commit -m "Update game platform"
-git remote add origin https://github.com/NOPE-BDB/nope.git
-git branch -M main
+git commit -m "Fix game list refresh issue"
 git push -u origin main --force
