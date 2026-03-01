840f1d31ef691c5ecb64c3a3030cc444ccdbf0f0
NOPE-BDB <3241410869@qq.com>
2026-03-01 22:48:04 +0800

    Fix scrollbar, follow button, register checks

--- push.ps1
+++ push.ps1
@@ -1,5 +1,5 @@
 cd c:\Users\witma\Documents\TRAE\railway
 $env:Path = "C:\Program Files\Git\bin;$env:Path"
 git add .
-git commit -m "Add file upload, cover, intro, rename menus"
+git commit -m "Fix scrollbar, follow button, register checks"
 git push https://github.com/NOPE-BDB/nope.git main --force
