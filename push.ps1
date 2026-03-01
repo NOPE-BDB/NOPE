036fc0334dd123c6a1678a31fece2485e4b34ec0
NOPE-BDB <3241410869@qq.com>
2026-03-01 23:26:39 +0800

    Fix follow button and modal color

--- push.ps1
+++ push.ps1
@@ -1,4 +1,4 @@
 cd c:\Users\witma\Documents\TRAE\railway
 git add .
-git commit -m "Fix follow button update"
+git commit -m "Fix follow button and modal color"
 git push https://github.com/NOPE-BDB/nope.git main --force
