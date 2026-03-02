224b6d43a1162e4d8fc65eb5cece977d883aa339
NOPE-BDB <3241410869@qq.com>
2026-03-02 13:45:36 +0800

    Add debug logs for follow

--- push.ps1
+++ push.ps1
@@ -1,4 +1,4 @@
 cd c:\Users\witma\Documents\TRAE\railway
 git add .
-git commit -m "Add server API for users and pending games"
+git commit -m "Add debug logs for follow"
 git push https://github.com/NOPE-BDB/nope.git main --force
